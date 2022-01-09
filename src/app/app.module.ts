4021fb9cfe004c20c9a9f58095af3c12d8b0ecd3
Junior <216026633@student.uj.ac.za>
2022-01-09 23:57:57 +0200

    Added header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AddButtonComponent } from './add-button/add-button.component';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddButtonComponent,
+    InputFieldComponent
   ],
   imports: [
     BrowserModule
