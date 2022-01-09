d772bcd63b02035666b4450b3b7079e7364f9a5c
Junior <216026633@student.uj.ac.za>
2022-01-10 01:07:15 +0200

    Added footer and footer child components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AddButtonComponent } from './add-button/add-button.component';
 import { InputFieldComponent } from './input-field/input-field.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AddButtonComponent,
-    InputFieldComponent
+    InputFieldComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
