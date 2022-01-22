faec2a0b112e4c47523fc7040b98179315723825
Junior <216026633@student.uj.ac.za>
2022-01-22 19:46:34 +0200

    created a function that adds checked tasks to an array

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../task';
+import { selectedTasks } from '../selected-tasks';
 
 @Component({
   selector: 'app-checkbox',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent implements OnInit {
-
+  @Input() task: any;
+  /* selectedtasks: Task[] = []; */
+  
   constructor() { }
   
   ngOnInit(): void {
+  }
+  
+  onSelect(event: any, task:Task){
+	  for(let i: number = 0; i < selectedTasks.length; i++){
+		  if(selectedTasks[i].id && task.id !== selectedTasks[i].id){
+			  selectedTasks.push(task);
+		  }
+	  }
+	  
+	  console.log(selectedTasks)
   }
 }
